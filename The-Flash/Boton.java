2bc444e60e52c103bf42f7913d369e9432a8dd0c
AngelMaldonado <amaldonadojuarez@gmail.com>
2020-12-31 21:44:00 -0600

    Clase Boton implementada

--- The-Flash/Boton.java
+++ The-Flash/Boton.java
@@ -1,14 +1,51 @@
 import greenfoot.Actor;
+import greenfoot.World;
+import greenfoot.MouseInfo;
+import java.util.ArrayList;
 
 /**
  * Esta clase es un boton que puede ser presionado dentro de un Menu o cualquier parte del juego
  * 
  * @Angel Maldonado (your name) 
- * @0.0
+ * @12/16/20
  */
-public class Boton extends Actor
+public abstract class Boton extends Actor
 {
-    public Boton()
+    /**
+     * DEFINIR CONSTANTES PARA EL DELAY Y LOS FRAMES DE SPRITEBOTON
+     */
+    private static final int framesBoton = 2;
+    private static final int delayBoton = 1;
+
+    private Sprite spriteBoton;
+    private int x;
+    private int y;
+
+    public void cargaBoton(ArrayList<String> nombresDeArchivos, int x, int y, World mundo)
     {
+        this.x = x;
+        this.y = y;
+        mundo.addObject(this, this.x, this.y);
+
+        spriteBoton = new SpriteDeActor(nombresDeArchivos, framesBoton, delayBoton, this);
+    }
+
+    public void muestraBoton(int frame) {
+        if(frame == 1) {
+            spriteBoton.fijaFrame(0);
+        } else {
+            spriteBoton.fijaFrame(1);
+        }
+    }
+
+    public boolean estaPresionado(MouseInfo infoDeMouse) {
+        if(infoDeMouse.getButton() == 1 &&
+            infoDeMouse.getX() > x && infoDeMouse.getX() < x + spriteBoton.obtenFrame(spriteBoton.obtenIterador()).getWidth() &&
+            infoDeMouse.getY() > y && infoDeMouse.getY() < y + spriteBoton.obtenFrame(spriteBoton.obtenIterador()).getHeight()) {
+            System.out.println("Boton presionado!");
+            return true;
+        } else {
+            return false;
+        }
     }
 }
